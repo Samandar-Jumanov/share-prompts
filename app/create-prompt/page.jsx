8707e04f970741ddc2a39e0640e2173410ef2d9e
Samandar-Jumanov <samandarjumanov055@gmail.com>
2024-01-30 12:14:54 -0500

    fullfilling createPost

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,16 +4,36 @@ import React , { useState} from 'react'
 import  Form  from '@components/Form'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 const CreatePrompt = () => {
   const [submitting , setSubmitting ] = useState(false);
   const [post , setPost ] = useState();
 
 
-  const createPost = (e) =>{
+  const createPost = async  (e) =>{
     e.preventDefault();
     setSubmitting(true);
+      try {
+        const res = await axios.post('/api/prompts', {
+          propmt : post?.propmt,
+          tag : post?.tag,
+          userId : session?.user.id
+        });
 
+        if(res.ok){
+             setSubmitting(false);
+             setPost(null);
+             router.push('/');
+        };
+        
+        router.push('/');
+      } catch (error) {
+        console.log(error?.message)
+      } finally{
+        setSubmitting(false);
+
+      }
   }
   return (
     <div>
@@ -26,6 +46,8 @@ const CreatePrompt = () => {
       />
        </div>
   )
-}
+
+
+  }
 
 export default CreatePrompt
